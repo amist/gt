7459aa050094d22b100d794f5264b7f3de24781b
Amitay Stern <stern.amitay@gmail.com>
2017-09-27 19:41:00 +0300

    Add pypy infrastructure

---
+++ run_pypy.py
@@ -0,0 +1,11 @@
+import sys
+
+base_dir = ''
+sys.path.insert(0, base_dir)
+sys.path.insert(0, base_dir + 'gt')
+sys.path.insert(0, base_dir + 'gt/tests')
+sys.path.insert(0, base_dir + 'gt/examples')
+
+# from gt.tests.test_tsp_evolve import run
+from test_tsp_evolve import run
+run()
